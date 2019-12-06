f14313512a190b6220230fe681962598b5b7419b
Venkatesh Achintalwar <venky@innov8.work>
2019-12-06 18:21:26 +0530

    removed marshmallow

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
-from flask_marshmallow import Marshmallow
+
 from flask_migrate import Migrate
 from datetime import datetime
 from config import Config
@@ -9,7 +9,7 @@ app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app, session_options={"expire_on_commit": False})
 
-ma = Marshmallow(app)
+
 migrate = Migrate(app, db)
 
 from app import routes
